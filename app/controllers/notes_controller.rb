b26816b64ab08b7b2afde2db918e4c8763b0a485
expsk <pavlovskes@gmail.com>
2013-11-08 22:46:10 +0300

    Notes wrap world

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -18,7 +18,7 @@ class NotesController < ApplicationController
     
     respond_to do |format| 
       if @note.save
-        format.html { redirect_to notes_path, notice: "Note was successfully created." }
+        format.html { redirect_to notes_path }
         format.json { @note }
         format.js {}
       else
@@ -35,7 +35,7 @@ class NotesController < ApplicationController
   def update
     respond_to do |format|
       if @note.update(note_params)
-        format.html { redirect_to notes_path, notice: "Note was successfully created." }
+        format.html { redirect_to notes_path }
         format.json { @note }
       else
         format.html { render action: 'edit' }
